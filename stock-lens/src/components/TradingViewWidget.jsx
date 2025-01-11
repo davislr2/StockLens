c2768304aa545719a91d892c9699d6244bb6238a
davislr2 <davisrluke@gmail.com>
2025-01-11 15:57:38 -0500

    stock prices and chart in a basic layout

--- stock-lens/src/components/TradingViewWidget.jsx
+++ stock-lens/src/components/TradingViewWidget.jsx
@@ -10,6 +10,7 @@ function TradingViewWidget() {
 
     // Create a new div element to hold the script
     const scriptContainer = document.createElement('div');
+    scriptContainer.id = "tradingview-widget-container";
     currentContainer.appendChild(scriptContainer);
 
     const script = document.createElement("script");
@@ -18,8 +19,6 @@ function TradingViewWidget() {
     script.async = true;
     script.innerHTML = `
       {
-        "width": "750",
-        "height": "1000",
         "symbol": "NASDAQ:AAPL",
         "interval": "D",
         "timezone": "Etc/UTC",
@@ -47,7 +46,6 @@ function TradingViewWidget() {
 
   return (
     <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
-      <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
       <div className="tradingview-widget-copyright">
         <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
           <span className="blue-text">Track all markets on TradingView</span>
