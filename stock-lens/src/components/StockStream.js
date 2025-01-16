23d3db3142a0473296387e244c2b7934dc9443c1
“davislr2” <davislr2@appstate.edu>
2025-01-16 12:35:38 -0500

    trying a new api

--- stock-lens/src/components/StockStream.js
+++ stock-lens/src/components/StockStream.js
@@ -1,49 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../styles/StockStream.css';
 
+const API_URL = "https://ekaedtgx37.execute-api.us-east-1.amazonaws.com/prod"; 
 
 const StockStream = () => {
     const [stocks, setStocks] = useState({});
 
     useEffect(() => {
-        const API_URL = 'https://7zb05ivko0.execute-api.us-east-1.amazonaws.com/Stage1';
-
-        const fetchStockData = async () => {
+        const fetchStocks = async () => {
             try {
-                const response = await fetch(API_URL, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        // Include any necessary authentication headers here
-                        // 'x-api-key': 'your-api-key',  // If using API Key
-                        // 'Authorization': 'Bearer your-token',  // If using bearer token
-                    },
-                });
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                const data = await response.json();
-                setStocks(data);
+                const response = await axios.get(API_URL);
+                const stockData = response.data.reduce((acc, stock) => {
+                    acc[stock.ticker] = stock;
+                    return acc;
+                }, {});
+                setStocks(stockData);
             } catch (error) {
                 console.error("Error fetching stock data:", error);
             }
         };
 
-        fetchStockData();
-        const interval = setInterval(fetchStockData, 3000);
+        fetchStocks();
+        const interval = setInterval(fetchStocks, 5000); // Refresh data every 5 seconds
+
         return () => clearInterval(interval);
     }, []);
 
     return (
-        <div id="stock-prices-container">
+       <div id="stock-prices-container">
             <h2 id="stock-prices-header">Live Stock Prices</h2>
             <ul id="stock-prices-list">
-                {Object.values(stocks).map(stock => (
+                {Object.values(stocks).map((stock) => (
                     <li key={stock.ticker} className="stock-price-list-item">
                         <strong>{stock.ticker}</strong>: ${stock.price.toFixed(2)} at {new Date(stock.timestamp * 1000).toLocaleTimeString()}
                     </li>
                 ))}
-            </ul>
-        </div>
+            </ul>    
+       </div> 
     );
 };
 
